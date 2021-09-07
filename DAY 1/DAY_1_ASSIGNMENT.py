f8dc5e779b51ff3c088c9fffeb463733a891636a
gaurav-163 <90236250+gaurav-163@users.noreply.github.com>
2021-09-07 10:45:24 +0530

    DAY 1 ASSIGNMENT SUBMITTED

---
+++ DAY 1/DAY_1_ASSIGNMENT.py
@@ -0,0 +1,9 @@
+#GAURAV CHAUDHARI
+#ASSIGNMENT 1 (DAY 1)
+a=10
+b=20
+print(int(a)*int(b))
+print(int(a)/int(b))
+print( int(a)%int(b))
+print((int(a)*int(b))+(int(a)/int(b)))
+
---
+++ DAY 1/DAY_1_SIMPLE_INTREST.py
@@ -0,0 +1,11 @@
+#Write a program which accepts principle,rate and time from user and print
+#the simple interest. The formula to calculate simple interest is :
+# = priciple*rate*time/ 100 solution
+print("Enter Principle value")
+Principle= input()
+print("Enter rate")
+Rate= input()
+print("Enter Time")
+Time= input()
+print("Simple interest:  " , (int (Principle)*int(Rate)*int(Time))/100)
+
